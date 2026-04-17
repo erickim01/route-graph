5aabce423a1c2ee16b032020002085feb974c34b
E K <kim73018@gmail.com>
2026-04-16 22:18:38 -0600

    Added core.py and temp settings.json

--- src/api_client.py
+++ src/api_client.py
@@ -1,5 +1,13 @@
 import requests
 
+### Wrapper to War API
+#
+# URL Construction
+#
+# Response Handling
+#
+# Static Caching
+
 BASE_URL = "https://war-service-live.foxholeservices.com/api/worldconquest"   #For now this goes to Able only
 
 def getWarData():       #May be generalized in behavior later to allow GETs to Baker and Charlie
@@ -7,8 +15,14 @@ def getWarData():       #May be generalized in behavior later to allow GETs to B
     r.raise_for_status()
     return r.json()
 
-def getMapData():       #May be generalized in behavior later to allow GETs to Baker and Charlie
+#Returns Valid Regions
+def getActiveRegions():       
     r = requests.get(f"{BASE_URL}/maps")
+    r.raise_for_status()
+    return r.json()
+
+def getMapData():       #May be generalized in behavior later to allow GETs to Baker and Charlie
+    r = requests.get(f"{BASE_URL}/maps/")
     r.raise_for_status()
     return r.json()
 
