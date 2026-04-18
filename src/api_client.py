0c59193d5095f67af005976f92610d8fa17e7159
E K <kim73018@gmail.com>
2026-04-17 21:23:26 -0600

    api_client is now a class

--- src/api_client.py
+++ src/api_client.py
@@ -10,28 +10,30 @@ import requests
 
 BASE_URL = "https://war-service-live.foxholeservices.com/api/worldconquest"   #For now this goes to Able only
 
-def getWarData():       #May be generalized in behavior later to allow GETs to Baker and Charlie
-    r = requests.get(f"{BASE_URL}/war")
-    r.raise_for_status()
-    return r.json()
+class FoxWarApiClient:
+    def get_war_data():       #May be generalized in behavior later to allow GETs to Baker and Charlie
+        r = requests.get(f"{BASE_URL}/war")
+        r.raise_for_status()
+        return r.json()
 
-#Returns Valid Regions
-def getActiveRegions():       
-    r = requests.get(f"{BASE_URL}/maps")
-    r.raise_for_status()
-    return r.json()
+    #Returns Valid Regions
+    def get_active_regions():       
+        r = requests.get(f"{BASE_URL}/maps")
+        r.raise_for_status()
+        return r.json()
 
-def getMapData():       #May be generalized in behavior later to allow GETs to Baker and Charlie
-    r = requests.get(f"{BASE_URL}/maps/")
-    r.raise_for_status()
-    return r.json()
+    def get_map_data():       #May be generalized in behavior later to allow GETs to Baker and Charlie
+        r = requests.get(f"{BASE_URL}/maps/")
+        r.raise_for_status()
+        return r.json()
 
 
-data = getMapData()
+
 
 # for key, value in data.items():
 #     print(f"{key}   -    Value: {value}")
 
-print(data)
+# data = get_map_data()
+# print(data)
 
 #warData['winner']
